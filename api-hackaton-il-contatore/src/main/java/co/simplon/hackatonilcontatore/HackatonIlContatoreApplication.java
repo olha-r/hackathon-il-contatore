091b4067f254d8e54f00ccee72c900a83245f4b4
Raulet Olha <raulet.olha@gmail.com>
2024-01-29 14:00:54 +0100

    first commit

---
+++ api-hackaton-il-contatore/src/main/java/co/simplon/hackatonilcontatore/HackatonIlContatoreApplication.java
@@ -0,0 +1,13 @@
+package co.simplon.hackatonilcontatore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackatonIlContatoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackatonIlContatoreApplication.class, args);
+	}
+
+}
---
+++ api-hackaton-il-contatore/src/test/java/co/simplon/hackatonilcontatore/HackatonIlContatoreApplicationTests.java
@@ -0,0 +1,13 @@
+package co.simplon.hackatonilcontatore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackatonIlContatoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
